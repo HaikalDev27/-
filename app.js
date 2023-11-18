9e5f42ecce9a7cd632c48c66dfb2b1c09cffa61b
HaikalDev27 <72543199+HaikalDev27@users.noreply.github.com>
2023-11-18 21:49:23 +0700

    DB update from local mysql

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-const express = require('express')
+const express = require("express")
+const mysql = require("mysql")
 
 const app = express()
 
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+const db = mysql.createConnection({
+    host:"localhost",
+    database:"target",
+    user:"root",
+    password:""
+})
+
+
+db.connect((err) => {
+    if (err) throw err
+    console.log("Server Target : 30 down")
+
+    const sql = "SELECT * FROM user"
+    db.query(sql, (err, result) => {
+        const users = JSON.parse(JSON.stringify(result))
+        console.log("Target list = ", users)
+        app.get("/", (req, res) => {
+          res.send(users)  
+        })
+    })
+
+    
+
+})
+
 app.listen(3000, () => {
-    console.log("Server ready")
+    console.log("Connected to Server:port3000")
 })
